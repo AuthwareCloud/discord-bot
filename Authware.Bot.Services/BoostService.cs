e1abb9b72a81b024836aeda779a4b2772fa4dc5a
Harry <59972716+dnlib@users.noreply.github.com>
2022-06-16 00:31:41 +0100

    Partially create server boost detection

--- Authware.Bot.Services/BoostService.cs
+++ Authware.Bot.Services/BoostService.cs
@@ -1,8 +1,31 @@
-﻿using Discord.WebSocket;
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
 
 namespace Authware.Bot.Services;
 
 public class BoostService
 {
-    private readonly DiscordSocketClient _client;
+    private readonly IConfiguration _configuration;
+    private readonly IHttpClientFactory _factory;
+
+    public BoostService(DiscordSocketClient client, IConfiguration configuration, IHttpClientFactory factory)
+    {
+        _configuration = configuration;
+        _factory = factory;
+        client.MessageReceived += MessageReceived;
+    }
+
+    private async Task MessageReceived(SocketMessage arg)
+    {
+        if (arg.Channel is not SocketGuildChannel channel) return;
+        var guildId = _configuration.GetValue<ulong>("GuildId");
+
+        if (arg.Type == MessageType.UserPremiumGuildSubscription && channel.Guild.Id == guildId)
+        {
+            using var client = _factory.CreateClient("authware");
+            
+            var response = await client.GetAsync()
+        }
+    }
 }
